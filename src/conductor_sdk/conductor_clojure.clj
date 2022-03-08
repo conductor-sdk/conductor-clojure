2a82b9a1815a085ccf7fb984114d9e3220df0086
James Stuart Milne <jstuartmilne@gmail.com>
2022-03-08 11:57:52 -0300

    feat: added thread-count option to options

--- src/conductor_sdk/conductor_clojure.clj
+++ src/conductor_sdk/conductor_clojure.clj
@@ -1,12 +1,11 @@
 (ns conductor-sdk.conductor-clojure
-(:import (com.netflix.conductor.client.worker Worker)
+  (:import (com.netflix.conductor.client.worker Worker)
            (io.orkes.conductor.client.http OrkesTaskClient)
            (com.netflix.conductor.client.automator TaskRunnerConfigurer$Builder)
            (com.netflix.conductor.common.metadata.tasks TaskResult TaskResult$Status))
 
-(:require [clojure.tools.logging :as log]
-          [conductor-sdk.utils :as utils])
-  )
+  (:require [clojure.tools.logging :as log]
+            [conductor-sdk.workers :as workers]))
 
 (defn task-client
   "Returns an instance of TaskClient. when app-key and app-secret are provided
@@ -18,16 +17,16 @@
       (.withCredentials client app-key app-secret)
       (log/debug "Creating client with authentication")
       )
-    client
-    ))
+    client))
 
 
 (defn task-runner-configurer
   "Returns a TaskRunnerConfigurer instance for given client and workers"
   ([client workers thread-count]
-  (-> (TaskRunnerConfigurer$Builder. client workers)
-      (.withThreadCount thread-count)
-      (.build)) )
+   (log/debug "Creating TaskRunnerConfigurer with thread-count" thread-count)
+   (-> (TaskRunnerConfigurer$Builder. client workers)
+       (.withThreadCount thread-count)
+       (.build)) )
   ([client workers] (task-runner-configurer client workers (count workers))))
 
 (defn runner-executor-for-workers
@@ -35,17 +34,26 @@
   TaskRunnerConfigurer instance"
   [workers options]
   (-> (task-client options)
-      (task-runner-configurer (map utils/clj-worker->Worker workers))
+      (task-runner-configurer
+       (map workers/clj-worker->Worker workers)
+       (->> options (merge {:thread-count (count workers)}) :thread-count))
       (doto (.init))))
+
 (comment
   (def worker
-{
-                    :name "clj_task"
-                    :execute (fn [someData]
-                               (println "This is imput data" (str someData))
-                               [:completed {:message "Hi From Clj"}])
-                    }
-    )
-(def instance (runner-executor-for-workers (list worker) {:url  "http://localhost:8080/api/" :app-key "f082aa94-ba42-4f95-9d9f-c808b3fd7485" :app-secret "JkSxYI8D8YIpcuNnSeBbPS9ug1rVPqii3Xia2nGRE1ICcepW"} ))
-(.shutdown instance)
-)
+    {
+     :name "clj_task"
+     :execute (fn [someData]
+                (println "This is imput data" (str someData))
+                [:completed {:message "Hi From Clj"}])
+     })
+
+  (def instance (runner-executor-for-workers
+                 (list worker)
+                 {
+                  :url  "http://localhost:8080/api/"
+                  :app-key "f082aa94-ba42-4f95-9d9f-c808b3fd7485"
+                  :app-secret "JkSxYI8D8YIpcuNnSeBbPS9ug1rVPqii3Xia2nGRE1ICcepW"
+                  :thread-count 2} ))
+  (.shutdown instance)
+  )
