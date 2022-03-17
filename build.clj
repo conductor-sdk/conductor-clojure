bdd391e077970d47045e56fc7ea9853213300076
James Stuart Milne <jstuartmilne@gmail.com>
2022-03-17 12:11:23 -0300

    refactor: use the correct group

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
             [org.corfield.build :as bb]))
 
-(def lib 'io.orkes.conductor/conductor-clojure)
+(def lib 'io.orkes/conductor-clojure)
 (def version "0.1.0-SNAPSHOT")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
