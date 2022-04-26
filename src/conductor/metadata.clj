f21743377738aa294bdf888542e1ea467dd06999
James Stuart Milne <jstuartmilne@gmail.com>
2022-04-26 15:54:32 -0300

    feat: added support for all metadata

--- src/conductor/metadata.clj
+++ src/conductor/metadata.clj
@@ -17,7 +17,8 @@
            (com.netflix.conductor.common.metadata.workflow WorkflowDef)
            (com.netflix.conductor.common.metadata.workflow WorkflowTask))
   (:require [clojure.tools.logging :as log]
-            [conductor.mapper-utils :as mapperutils]))
+            [conductor.mapper-utils :as mapperutils]
+            [conductor.metadata :as metadata]))
 
 (defn metadata-client
   "Given a map with options creates a metadata-client"
@@ -26,10 +27,8 @@
     (.setRootURI client url)
     (when app-key
       (.withCredentials client app-key app-secret)
-      (log/debug "Creating client with authentication")
-      )
+      (log/debug "Creating client with authentication"))
     client))
-
 
 (defn register-tasks-using-client
   "Given a client instance and a list of tasks,
@@ -44,8 +43,8 @@
       (metadata-client)
       (register-tasks-using-client tasks)))
 
-(defn workflow-def-using-client
-  "Takes a client and a workflow definition in EDN, will register a worflow in conductor"
+(defn register-workflow-def-using-client
+  "Takes a client and a workflow definition in edn, will register a worflow in conductor"
   [client workflow]
   (.registerWorkflowDef client (mapperutils/clj-workflow->WorkflowDef workflow)))
 
@@ -54,4 +53,95 @@
   [options workflow]
 (-> options
       (metadata-client)
-      (workflow-def-using-client workflow)))
+      (register-workflow-def-using-client workflow)))
+
+(defn update-workflows-def-using-client
+  "takes a client and a list of workflows definition in edn, will update all workflows in list"
+  [client workflows]
+  (.updateWorkflowDefs client (map mapperutils/clj-workflow->WorkflowDef workflows)))
+
+(defn update-workflows-def
+  "Takes a map of options, and a list of workflow definitions. will update every workflow on the list"
+  [options workflows]
+  (-> options
+      (metadata-client)
+      (update-workflows-def-using-client workflows)))
+
+(defn get-workflow-def-using-client
+  "Takes a client a name and a version. Will fetch for workflow definition"
+  ([client name version]
+   (mapperutils/java-map->clj (.getWorkflowDef client name (int version))) )
+  ([client name]
+   (mapperutils/java-map->clj (.getWorkflowDef client name 1))))
+
+(defn get-workflow-def
+  "Takes a map of options, a name and a version. Will fetch for workflow definition"
+  [options name version]
+(-> options
+      (metadata-client)
+      (get-workflow-def-using-client name version)))
+
+
+(defn unregister-workflow-def-using-client
+  "Takes a client a name and a version. will unregister workflow. returns nil on success"
+  [client name version]
+(.unregisterWorkflowDef client name (int version)))
+
+(defn unregister-workflow-def
+  "Takes a map of options, a name and a version. will unregister workflow. returns nil on success"
+  [options name version]
+  (-> options
+      (metadata-client)
+      (unregister-workflow-def-using-client name version)))
+
+(defn update-task-definition-with-client [client task-definition]
+  (.updateTaskDef client (mapperutils/clj-task->TaskDef task-definition)))
+
+(defn update-task-definition
+  "Takes a map of options, and a list of workflow definitions. will update every workflow on the list"
+  [options task-definition]
+  (-> options
+      (metadata-client)
+      (update-task-definition-with-client task-definition)))
+
+(defn get-task-def-with-client [client task-ref]
+  (mapperutils/java-map->clj (.getTaskDef client task-ref) ))
+
+(defn get-task-def [options task-def]
+[options task-def]
+  (-> options
+      (metadata-client)
+      (get-task-def-with-client task-def)))
+
+
+(defn unregister-task-with-client [client task-ref]
+  (.unregisterTaskDef client task-ref))
+
+(defn unregister-task
+[options task-ref]
+  (-> options
+      (metadata-client)
+      (unregister-task-with-client task-ref)))
+
+(comment
+(def options {
+              :app-key "c38bf576-a208-4c4b-b6d3-bf700b8e454d"
+              :app-secret "Z3YUZurKtJ3J9CqrdbRxOyL7kUqLrUGR8sdVknRUAbyGqean"
+              :url "http://localhost:8080/api/"
+              })
+(def wf (get-workflow-def options "simple_wf" 1) )
+
+(:tasks wf)
+(register-workflow-def options (assoc wf :version 29))
+
+(unregister-workflow-def options "exclusive_join" 1)
+
+(def some-task (get-task-def options "cool_clj_task_b") )
+
+(update-task-definition options (assoc some-task :owner-email "othermaila@mail.com") )
+
+(unregister-task options "cool_clj_task_b")
+
+
+)
+
--- test/conductor/conductor_clojure_test.clj
+++ test/conductor/conductor_clojure_test.clj
@@ -13,20 +13,95 @@
   (:require [clojure.test :refer :all]
             [conductor.client :refer :all]
             [conductor.metadata :as metadata]
-            ))
+            [conductor.workflow-resource :as wresource]))
 (def options {
               :url  "http://localhost:8080/api/"
               } )
 
+
 (deftest workflow-creation
+  (def cool-b-task {
+                    :name "cool_clj_task_b"
+                    :description "some description"
+                    :owner-email "mail@gmail.com"
+                    :retry-count 3
+                    :timeout-seconds 300
+                    :response-timeout-seconds 180
+                    } )
+
+  (def exclusive-join-workflow
+    {
+     :name "exclusive_join"
+     :description "Exclusive Join Example"
+     :version 1
+     :tasks [ {
+               :name "api_decision"
+               :task-reference-name "api_decision_ref"
+               :input-parameters {
+                                  "case_value_param" "${workflow.input.type}"
+                                  }
+               :type :decision
+               :case-value-param "case_value_param"
+               :default-case []
+               :decision-cases {
+                                "POST" [{
+                                         :name "get-posts"
+                                         :task-reference-name "get_posts_ref"
+                                         :input-parameters {
+                                                            "http_request" {
+                                                                            "uri" "https://jsonplaceholder.typicode.com/posts/1"
+                                                                            "method" "GET"
+                                                                            }
+                                                            }
+                                         :type :http
+                                         }]
+                                "COMMENT" [{
+                                            :name "get_posts_comments"
+                                            :task-reference-name "get_post_comments_ref"
+                                            :input-parameters {
+                                                               "http_request" {
+                                                                               "uri" "https://jsonplaceholder.typicode.com/comments?postId=1"
+                                                                               "method" "GET"
+                                                                               }
+
+                                                               }
+                                            :type :http
+                                            }]
+                                "USER" [{
+                                         :name "get_user_posts"
+                                         :task-reference-name "get_user_posts_ref"
+                                         :input-parameters {
+                                                            "http_request" {
+                                                                            "uri" "https://jsonplaceholder.typicode.com/posts?userId=1"
+                                                                            "method" "GET"
+                                                                            }
+
+                                                            }
+
+                                         :type :http
+                                         }]
+                                }
+               },
+             {
+              :name "notification_join",
+              :task-reference-name "notification_join_ref"
+              :input-parameters {}
+              :type :exclusive-join
+              :join-on ["get_posts_ref" "get_post_comments_ref" "get_user_posts_ref"]
+              }
+
+             ]
+     :input-parameters []
+     :output-parameters {:message "${clj_prog_task_ref.output.:message}"}
+     :schema-version 2
+     :restartable true
+     :owner-email "mail@yahoo.com"
+     :timeout-seconds 0
+     :timeout-policy :alert-only
+     })
+
   (testing "Can register multiple tasks at once"
-    (is (= nil (metadata/register-tasks options [{
-                                                  :name "cool_clj_task_b"
-                                                  :description "some description"
-                                                  :owner-email "mail@gmail.com"
-                                                  :retry-count 3
-                                                  :timeout-seconds 300
-                                                  :response-timeout-seconds 180 },
+    (is (= nil (metadata/register-tasks options [cool-b-task,
                                                  {
                                                   :name "cool_clj_task_z"
                                                   :description "some description"
@@ -52,9 +127,9 @@
                                                                   :task-reference-name "cool_clj_task_ref"
                                                                   :input-parameters {}
                                                                   :type :simple
-                                                                  },
+                                                                  }
                                                                 {
-                                                                 :name "someting",
+                                                                 :name "something",
                                                                  :task-reference-name "other"
                                                                  :input-parameters {}
                                                                  :type :fork-join
@@ -93,75 +168,7 @@
                                                         })))
     )
   (testing  "Can create a workflow with exclusive-join"
-    (is (= nil (metadata/register-workflow-def options {
-                                                        :name "exclusive_join"
-                                                        :description "Exclusive Join Example"
-                                                        :version 1
-                                                        :tasks [ {
-                                                                  :name "api_decision"
-                                                                  :task-reference-name "api_decision_ref"
-                                                                  :input-parameters {
-                                                                                     "case_value_param" "${workflow.input.type}"
-                                                                                     }
-                                                                  :type :decision
-                                                                  :case-value-param "case_value_param"
-                                                                  :default-case []
-                                                                  :decision-cases {
-                                                                                   "POST" [{
-                                                                                            :name "get-posts"
-                                                                                            :task-reference-name "get_posts_ref"
-                                                                                            :input-parameters {
-                                                                                                               "http_request" {
-                                                                                                                               "uri" "https://jsonplaceholder.typicode.com/posts/1"
-                                                                                                                               "method" "GET"
-                                                                                                                               }
-                                                                                                               }
-                                                                                            :type :http
-                                                                                            }]
-                                                                                   "COMMENT" [{
-                                                                                               :name "get_posts_comments"
-                                                                                               :task-reference-name "get_post_comments_ref"
-                                                                                               :input-parameters {
-                                                                                                                  "http_request" {
-                                                                                                                                  "uri" "https://jsonplaceholder.typicode.com/comments?postId=1"
-                                                                                                                                  "method" "GET"
-                                                                                                                                  }
-
-                                                                                                                  }
-                                                                                               :type :http
-                                                                                               }]
-                                                                                   "USER" [{
-                                                                                            :name "get_user_posts"
-                                                                                            :task-reference-name "get_user_posts_ref"
-                                                                                            :input-parameters {
-                                                                                                               "http_request" {
-                                                                                                                               "uri" "https://jsonplaceholder.typicode.com/posts?userId=1"
-                                                                                                                               "method" "GET"
-                                                                                                                               }
-
-                                                                                                               }
-
-                                                                                            :type :http
-                                                                                            }]
-                                                                                   }
-                                                                  },
-                                                                {
-                                                                 :name "notification_join",
-                                                                 :task-reference-name "notification_join_ref"
-                                                                 :input-parameters {}
-                                                                 :type :exclusive-join
-                                                                 :join-on ["get_posts_ref" "get_post_comments_ref" "get_user_posts_ref"]
-                                                                 }
-
-                                                                ]
-                                                        :input-parameters []
-                                                        :output-parameters {:message "${clj_prog_task_ref.output.:message}"}
-                                                        :schema-version 2
-                                                        :restartable true
-                                                        :owner-email "mail@yahoo.com"
-                                                        :timeout-seconds 0
-                                                        :timeout-policy :alert-only
-                                                        })))
+    (is (= nil (metadata/register-workflow-def options exclusive-join-workflow)))
     )
   (testing "Can create a workflow with loop and decision"
     (is (= nil (metadata/register-workflow-def options {
@@ -244,12 +251,83 @@
                                                                  :input-parameters {}
                                                                  :type :wait
                                                                  }
-                                                                ]
-                                                        })))
-    )
+                                                                ]}))))
+
   (testing "Should be able to start a workflow"
-    (is (not-empty (start-workflow options {:version 1 :input {} :name "cool_clj_workflow_2"} )) )
+    (let [wf-execution-id (wresource/start-workflow options {:version 1 :input {} :name "cool_clj_workflow_2"}) ]
+      (is (not-empty wf-execution-id) )
+      (testing "Should be able to get workflow execution"
+        (is not-empty (wresource/get-workflow options wf-execution-id))
+        )
+      )
     )
+
+  (testing "Should be able to get workflow defintion"
+    (let [workflow-name (:name exclusive-join-workflow)
+          workflow-version (:version exclusive-join-workflow)
+          workflow-defintion (metadata/get-workflow-def options workflow-name 1)]
+      (is (nil? (metadata/register-workflow-def options (assoc workflow-defintion :version (inc workflow-version) ))))
+      (testing "Should be able to unregister a workflow"
+        (is (nil? (metadata/unregister-workflow-def options workflow-name workflow-version))))
+      )
+    )
+
+  (testing "Should be able to get a task definition by name"
+    (let [task-name (:name cool-b-task)
+          existing-task (metadata/get-task-def options task-name)]
+      (is (not-empty existing-task))
+      (testing "Should be able to update task properties"
+        (is  (nil? (metadata/update-task-definition options (assoc existing-task :owner-email "othermaila@mail.com") ) ) )
+        )
+      (testing "Should be able to unregister task"
+        (is (nil? (metadata/unregister-task options task-name)))
+        ))))
+
+(comment
+
+(metadata/register-workflow-def options {
+                                                        :name "wf_to_wait"
+                                                        :description "created programatically from clj"
+                                                        :version 1
+                                                        :tasks [ {
+                                                                                :name "cool_clj_task_z"
+                                                                                :task-reference-name "cool_clj_task_z_ref"
+                                                                                :input-parameters {}
+                                                                                :type :simple
+                                                                  },
+                                                                {
+                                                                                :name "cool_clj_task_x"
+                                                                                :task-reference-name "cool_clj_task_x_ref"
+                                                                                :input-parameters {}
+                                                                                :type :simple
+                                                                                }
+                                                                ]
+                                                        :input-parameters []
+                                                        :output-parameters { "message" "${cool_clj_task_x_ref.output.message}"}
+                                                        :schema-version 2
+                                                        :restartable true
+                                                        :owner-email "mail@yahoo.com"
+                                                        :timeout-seconds 0
+                                                        :timeout-policy :alert-only
+                                                        })
+(def instance (runner-executor-for-workers [{:name "cool_clj_task_z"
+                                             :execute (fn [d]
+                                                        (Thread/sleep 5000)
+                                                        [:completed {"message" "Something silly"}]
+                                                        )
+                                             }
+                                            {:name "cool_clj_task_x"
+                                             :execute (fn [d]
+                                                        (Thread/sleep 5000)
+                                                        [:completed {"message" "Something silly 2"}]
+                                                        )
+                                             }
+                                            ] options))
+  (.shutdown instance)
+
+  (def wf-id (wresource/start-workflow options {:version 1 :input {} :name "wf_to_wait"}) )
+
+  (wresource/terminate-workflow options wf-id)
 
 
   )
