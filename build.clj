729d07ef04785ea9f8efd410beb02810c82b233b
James Stuart Milne <jstuartmilne@gmail.com>
2022-05-13 19:11:27 -0300

    feat: upsert version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'io.orkes/conductor-clojure)
-(def version "orkes-0.1.6-SNAPSHOT")
+(def version "0.1.7")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
